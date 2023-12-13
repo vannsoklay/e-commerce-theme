8f168a1faff1c790e26c315d2a77274e02cf25c9
vannsoklay <soklayvann@gmail.com>
2023-12-14 00:26:07 +0700

    add-vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import solid from "solid-start/vite";
-import { defineConfig } from "vite";
 
-export default defineConfig({
-  server: {
-    port: 2002
-  },
-  plugins: [solid()],
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd());
+  const PORT = `${env.VITE_PORT ?? '1024'}`;
+
+  return {
+    server: {
+      port: parseInt(PORT),
+    },
+    build: {
+      target: "esnext",
+    },
+    plugins: [solid()],
+  };
 });
