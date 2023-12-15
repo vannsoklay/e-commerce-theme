b39696fa5e606d41d20f948ec58a383f6219fa4c
vannsoklay <soklayvann@gmail.com>
2023-12-15 20:11:27 +0700

    remove-vite

--- vite.config.ts
+++ vite.config.ts
@@ -16,24 +16,3 @@ export default defineConfig(({ mode }: ConfigEnv) => {
     plugins: [solid()],
   };
 });
-
-
-// import solidPlugin from "vite-plugin-solid";
-// import viteTsconfigPaths from 'vite-tsconfig-paths';
-
-// import { ConfigEnv, defineConfig, loadEnv } from 'vite';
-
-// export default defineConfig(({ mode }: ConfigEnv) => {
-//   const env = loadEnv(mode, process.cwd());
-//   const PORT = `${env.VITE_PORT ?? '1024'}`;
-
-//   return {
-//     server: {
-//       port: parseInt(PORT),
-//     },
-//     build: {
-//       target: "esnext",
-//     },
-//     plugins: [viteTsconfigPaths(), solidPlugin()],
-//   };
-// });
